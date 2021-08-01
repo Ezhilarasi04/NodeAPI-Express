2cde9c28f504fd4c1c008c44e9eebdd1ec16b2ad
Ezhilarasi S <ezhilarasisenthilkumar13@gmail.com>
2021-08-01 17:59:11 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,36 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use (cors());
+/*app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});*/
 
 app.get('/add/:n1/:n2', (req, res) => {
     var num1 = Number(req.params.n1);
     var num2 = Number(req.params.n2);
     var sum = num1+num2;
     var result = String(sum);
-    var inJs = {"number1" : num1, "number2" : num2, "result" : result};
+    var inJs = {"res" : result};
     var inJson = JSON.stringify(inJs);
-    res.send(inJson); 
+    //res.json({inJson}); 
+    res.send(inJson);
 });
 
 app.get('/subtract/:n1/:n2', (req, res) => {
@@ -19,7 +39,7 @@ app.get('/subtract/:n1/:n2', (req, res) => {
     var num2 = Number(req.params.n2);
     var sub = num1-num2;
     var result = String(sub);
-    var inJs = {"number1" : num1, "number2" : num2, "result" : result};
+    var inJs = {"res" : result};
     var inJson = JSON.stringify(inJs);
     res.send(inJson); 
 });
@@ -29,7 +49,7 @@ app.get('/multiply/:n1/:n2', (req, res) => {
     var num2 = Number(req.params.n2);
     var mul = num1*num2;
     var result = String(mul);
-    var inJs = {"number1" : num1, "number2" : num2, "result" : result};
+    var inJs = {"res" : result};
     var inJson = JSON.stringify(inJs);
     res.send(inJson); 
 });
@@ -39,12 +59,12 @@ app.get('/divide/:n1/:n2', (req, res) => {
     var num2 = Number(req.params.n2);
     var div = num1/num2;
     var result = String(div);
-    var inJs = {"number1" : num1, "number2" : num2, "result" : result};
+    var inJs = {"res" : result};
     var inJson = JSON.stringify(inJs);
     res.send(inJson); 
 });
 
 
 //PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log('Listening on port %s..', +port));
